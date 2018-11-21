a1702dc297dfdede45bb87ba993107479bc6f4e9
cnsgithub <cnsgithub@users.noreply.github.com>
2018-11-21 16:56:53 +0100

    add test to verify if myfaces handles illegal xml characters properly

--- src/main/java/main/TestBean.java
+++ src/main/java/main/TestBean.java
@@ -3,11 +3,13 @@ package main;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 @Named
 @RequestScoped
 public class TestBean {
 	public static String illegalXmlChar = "\u000C";
+	private static String illegalXmlChars = getIllegalXmlChars();
 
 	private String input;
 
@@ -16,7 +18,7 @@ public class TestBean {
 	}
 
 	public String getMessage() {
-		return getRandom() + illegalXmlChar;
+		return getRandom() + illegalXmlChars;
 	}
 
 
@@ -30,5 +32,26 @@ public class TestBean {
 
 	public void setInput(String input) {
 		this.input = input;
+	}
+
+	public static String getIllegalXmlChars() {
+		StringBuilder illegal = new StringBuilder();
+		for(char c = Character.MIN_VALUE; c < Character.MAX_VALUE; c++) {
+			// character ranges from https://www.w3.org/TR/REC-xml/#charsets
+			if(c == '\u0009' || c == '\n' || c == '\r')
+				continue;
+			if(c > '\u0020' && c < '\uD7FF')
+				continue;
+			if(c > '\uE000' && c < '\uFFFD')
+				continue;
+			illegal.append(c);
+		}
+		var out =  illegal.toString();
+		System.out.println(out);
+		return out;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getIllegalXmlChars().length());
 	}
 }
